ca800719006e19ed9b7a8893deda96887d997bf6
Nario <oliwka1416@gmail.com>
2024-03-19 23:46:52 -0700

    added staff CPT, changed staff CPT block editor, added taxonomies

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -47,6 +47,55 @@ function schoolsite_register_custom_post_types() {
         'supports'           => array( 'title', 'thumbnail', 'editor' ),
     );
     register_post_type( 'schoolsite-student', $args );
+
+    // Staff CPT
+
+    $labels = array(
+        'name'               => _x( 'Staff', 'post type general name' ),
+        'singular_name'      => _x( 'Staff', 'post type singular name'),
+        'menu_name'          => _x( 'Staff', 'admin menu' ),
+        'name_admin_bar'     => _x( 'Staff', 'add new on admin bar' ),
+        'add_new'            => _x( 'Add New', 'staff' ),
+        'add_new_item'       => __( 'Add New Staff' ),
+        'new_item'           => __( 'New Staff' ),
+        'edit_item'          => __( 'Edit Staff' ),
+        'view_item'          => __( 'View Staff' ),
+        'all_items'          => __( 'All Staff' ),
+        'search_items'       => __( 'Search Staff' ),
+        'parent_item_colon'  => __( 'Parent Staff:' ),
+        'not_found'          => __( 'No Staff found.' ),
+        'not_found_in_trash' => __( 'No Staff found in Trash.' ),
+        'archives'           => __( 'Staff Archives'),
+        'insert_into_item'   => __( 'Insert into Staff'),
+        'uploaded_to_this_item' => __( 'Uploaded to this Staff'),
+        'filter_item_list'   => __( 'Filter Staff list'),
+        'items_list_navigation' => __( 'Staff list navigation'),
+        'items_list'         => __( 'Staff list'),
+        'featured_image'     => __( 'Staff featured image'),
+        'set_featured_image' => __( 'Set Staff featured image'),
+        'remove_featured_image' => __( 'Remove Staff featured image'),
+        'use_featured_image' => __( 'Use as featured image'),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_nav_menus'  => true,
+        'show_in_admin_bar'  => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'staff' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'menu_icon'          => 'dashicons-archive',
+        'supports'           => array( 'title', 'thumbnail', 'editor' ),
+    );
+    register_post_type( 'schoolsite-staff', $args );
 }
 
 add_action( 'init', 'schoolsite_register_custom_post_types' );
