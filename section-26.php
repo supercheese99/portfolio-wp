42274e722bf32d358f4f5cd80988e55c46b4c2fa
oliwia <wegneroliwia@outlook.com>
2024-12-20 19:54:12 -0700

    added stars

--- section-26.php
+++ section-26.php
@@ -1,10 +1,11 @@
 <!-- section 0 - landing page -->
- 
-<!-- <div class="image-container">
-    <img src="<?php echo get_template_directory_uri(); ?>/media/avatar.jpg" alt="avatar of Oliwia" />
-</div> -->
+
+<div><img src="<?php echo get_template_directory_uri(); ?>/media/stars.png" alt="stars" class="stars star-1">
+</div>
 
 <div class="text-container">
     <h1 id="name">Oliwia Wegner</h1>
     <p id="title">Web Developer & Designer</p>
 </div>
+
+<div><img src="<?php echo get_template_directory_uri(); ?>/media/stars.png" alt="stars" class="stars star-2"></div>
